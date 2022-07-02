b6a860ddf6e8850f1f4978c3c9df77dfea236245
oliverzi <oliver.zivcic@gmail.com>
2022-07-02 16:40:17 +0200

    Added recognition of other major cards.

--- luhn-algorithm/luhn-algorithm.cpp
+++ luhn-algorithm/luhn-algorithm.cpp
@@ -21,8 +21,16 @@ int main()
 
     sprintf_s(a, "%f", x);
 
+    // 0 = 48, 1 = 49, 2 = 50, 3 = 51, 4 = 52, 5 = 53, 6 = 54, 7 = 55, 8 = 56, 9 = 57
+
     if(a[0]==52)
         printf_s("The credit card is valid. It is issued by Visa.");
+    if (a[0] == 53 && (a[1]>=49 && a[1]<=53))
+        printf_s("The credit card is valid. It is issued by Mastercard.");
+    if (a[0] == 54 && ((a[1] == 53) || (a[1]==48 && a[2]==49 && a[3]==49) || (a[1]==52 && a[2]==52)))
+        printf_s("The credit card is valid. It is issued by Discover.");
+    if (a[0] == 51 && (a[1]==52 || a[1]==55))
+        printf_s("The credit card is valid. It is issued by American Express (Amex).");
 
     return 0;
 }
