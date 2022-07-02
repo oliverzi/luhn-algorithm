f4cda7833e0957ba82f44e798670d85aafec4094
oliverzi <oliver.zivcic@gmail.com>
2022-07-02 22:27:53 +0200

    Improved integer array.

--- luhn-algorithm/luhn-algorithm.cpp
+++ luhn-algorithm/luhn-algorithm.cpp
@@ -4,44 +4,23 @@
 
 int main()
 {
-    int i, e = 0, n = 0;
-    float x;
-    char a[50];
+    int i, n=0;
+    long long x;
+    long long a[16];
 
     while ((n < 16) || (n > 16)) {
         printf_s("Enter a credit card number: ");
-        scanf_s("%f", &x);
+        scanf_s("%lld", &x);
         n = floor(log10(abs(x))) + 1;
         if ((n < 16) || (n > 16)) {
             printf_s("The entered credit card number doesn't have 16 digits!\n");
         }
     }
 
-    sprintf_s(a, "%f", x);
-
-    // 0 = 48, 1 = 49, 2 = 50, 3 = 51, 4 = 52, 5 = 53, 6 = 54, 7 = 55, 8 = 56, 9 = 57
-
-    if(a[0]==52){
-        printf_s("The credit card is valid. It is issued by Visa.");
-        e++;
+    for (i = 15; i >= 0; i--) {
+        a[i] = x % 10;
+        x = x / 10;
     }
-    if (a[0] == 53 && (a[1] >= 49 && a[1] <= 53)) {
-        printf_s("The credit card is valid. It is issued by Mastercard.");
-        e++;
-    }
-    if (a[0] == 54 && ((a[1] == 53) || (a[1] == 48 && a[2] == 49 && a[3] == 49) || (a[1] == 52 && a[2] == 52))) {
-        printf_s("The credit card is valid. It is issued by Discover.");
-        e++;
-    }
-    if (a[0] == 51 && (a[1] == 52 || a[1] == 55)) {
-        printf_s("The credit card is valid. It is issued by American Express (Amex).");
-        e++;
-    }
-    if (e == 0) {
-        printf_s("The credit card has not been recognized.");
-    }
-
-
 
     return 0;
 }
